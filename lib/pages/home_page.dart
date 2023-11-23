3b9badbb096c3b06061401c38290c22d4615a8a6
Martin Villa <martin.villa0703@gmail.com>
2023-11-22 21:57:16 -0500

    Configuracion general

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,100 +1,100 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_codigo_sp/widgets/my_drawer_widget.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
+class _HomePageState extends State<HomePage> {
+  bool isDarkMode = false;
+  int gender = 1;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Shared preferens app"),
       ),
-      drawer: Drawer(
+      drawer: MyDrawerWidget(),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
         child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            DrawerHeader(
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  fit: BoxFit.cover,
-                  image: NetworkImage(
-                    "https://images.pexels.com/photos/1269968/pexels-photo-1269968.jpeg?auto=compress&cs=tinysrgb&w=600",
-                  ),
+            Text(
+              "Configuracion General",
+              style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(
+              height: 12.0,
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: "Nombre Completo"),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            TextField(decoration: InputDecoration(hintText: "Dirección")),
+            const SizedBox(
+              height: 12.0,
+            ),
+            // Switch(
+            //   value: false,
+            //   onChanged: (bool value) {},
+            // ),
+            SwitchListTile(
+              value: isDarkMode,
+              title: Text("Modo oscuro"),
+              onChanged: (bool value) {
+                print(value);
+                isDarkMode = value;
+                setState(() {});
+              },
+            ),
+            // Radio(
+            //  value: 1,
+            //  groupValue: 1,
+            //  onChanged: (int? value) {},
+            //),
+            RadioListTile(
+              value: 1,
+              groupValue: gender,
+              title: Text("Masculino"),
+              onChanged: (int? value) {
+                gender = value!;
+                setState(() {});
+              },
+            ),
+            RadioListTile(
+              value: 2,
+              groupValue: gender,
+              title: Text("Femenino"),
+              onChanged: (int? value) {
+                gender = value!;
+                setState(() {});
+              },
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 50,
+              child: ElevatedButton.icon(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.black,
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12.0))),
+                icon: Icon(
+                  Icons.save,
+                  color: Colors.white,
                 ),
+                label: Text("Guarda data"),
               ),
-              child: SizedBox(
-                width: double.infinity,
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    CircleAvatar(
-                      backgroundColor: Colors.white12,
-                      radius: 30,
-                      backgroundImage: NetworkImage(
-                        "https://images.pexels.com/photos/240561/pexels-photo-240561.jpeg?auto=compress&cs=tinysrgb&w=600",
-                      ),
-                    ),
-                    Text(
-                      "Martin",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 16,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                    Text(
-                      "Administrador",
-                      style: TextStyle(
-                        color: Colors.white,
-                        fontSize: 14,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.people,
-                color: Colors.black.withOpacity(0.6),
-              ),
-              title: Text("Mi perfil"),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.file_copy,
-                color: Colors.black.withOpacity(0.6),
-              ),
-              title: Text("Portafolio General"),
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.lock,
-                color: Colors.black.withOpacity(0.6),
-              ),
-              title: Text("Cambiar contraseña"),
-            ),
-            Divider(
-              indent: 12,
-              endIndent: 12,
-            ),
-            ListTile(
-              leading: Icon(
-                Icons.exit_to_app,
-                color: Colors.black.withOpacity(0.6),
-              ),
-              title: Text("Salir del App"),
             ),
           ],
         ),
-      ),
-      body: Column(
-        children: [
-          Row(
-            children: [],
-          ),
-        ],
       ),
     );
   }
